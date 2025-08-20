d2fb237bf1167c041aa3080c97ebc610791ee5aa
hmajr <hamjr.tec@gmail.com>
2025-08-20 17:57:16 +0100

    Create function to add expense item

--- scripts.js
+++ scripts.js
@@ -31,4 +31,50 @@ form.onsubmit = (event) => {
     amount : amount.value,
     created_at : new Date(),
   }
+
+  expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense){
+  try {
+    let expenseItem = document.createElement("li")
+    expenseItem.classList.add("expense")
+    
+    // Expense category icon
+    let expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `./img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+    
+    // Expense info, Name and Category
+    let expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    let expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    let expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+    expenseInfo.append(expenseName, expenseCategory)
+
+
+    // Expense amount
+    let expenseAmount = document.createElement("span")
+    expenseAmount.classList.add("expense-amount")
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+                                                  .toUpperCase()
+                                                  .replace("R$","")}`
+    
+    // Delete button
+    let expenseDelete = document.createElement("img")
+    expenseDelete.classList.add("remove-icon")
+    expenseDelete.setAttribute("src", "./img/remove.svg")
+    expenseDelete.setAttribute("alt", "Remove item")
+
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, expenseDelete)
+
+    expenseList.appendChild(expenseItem)
+    updateTotals()
+  } catch (error) {
+  }
+}
 }
